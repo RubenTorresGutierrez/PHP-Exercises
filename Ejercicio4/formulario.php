6bdf94582a2fac7a46c4e23fb56f10c4122d3e30
Ruben Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-09-24 15:59:24 +0200

    V3

--- Ejercicio4/formulario.php
+++ Ejercicio4/formulario.php
@@ -7,11 +7,11 @@
     <body>
         <h1>DATOS RECIBIDOS:</h1>
         <?php
-            //Variables
 
             echo 'Con print_r';
             if(isset($_GET))
                 print_r($_GET);
+            echo '<br/>';
 
             if(!empty($_GET['correo']))
                 echo 'El correo es '.$_GET['correo'].'<br/>';
@@ -21,12 +21,17 @@
                 echo 'La contraseña es '.$_GET['pw'].'<br/>';
             if(isset($_GET['genero']))
                 echo 'El género es '.$_GET['genero'].'<br/>';
-            if(isset($_GET['intereses']))
-                echo 'Los intereses son '.$_GET['intereses'].'<br/>';
+            if(isset($_GET['intereses'])){
+                echo 'Los intereses son';
+                foreach($_GET['intereses'] as $intereses)
+                    echo ', '.$intereses;
+                echo '<br/>';
+            }
             if(!empty($_GET['conocer']))
                 echo 'Nos conoció a través de '.$_GET['conocer'].'<br/>';
             if(!empty($_GET['comentarios']))
                 echo 'Mensaje: '.$_GET['comentarios'].'<br/>';
+
         ?>
     </body>
 </html>
